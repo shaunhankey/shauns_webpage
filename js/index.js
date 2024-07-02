f983e4d4a81573c939580c9f8a101b0890d7569e
shaun <shaunhankey@gmail.com>
2024-07-02 20:39:15 +0100

    update

--- js/index.js
+++ js/index.js
@@ -1,24 +1,29 @@
-
 function startTime() {
-        var today = new Date();
-        var h = today.getHours();
-        var m = today.getMinutes();
-        var s = today.getSeconds();
-        h = checkTime(h);
-        m = checkTime(m);
-        s = checkTime(s);
-        document.getElementById('clock').innerHTML = h + ":" + m + ":" + s;
-        var t = setTimeout(startTime, 500);
+    var today = new Date();
+    var endOfYear = new Date(today.getFullYear(), 11, 31, 23, 59, 59);
+    var timeDiff = endOfYear - today;
+
+    var days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+
+    hours = checkTime(hours);
+    minutes = checkTime(minutes);
+    seconds = checkTime(seconds);
+
+    document.getElementById('clock').innerHTML = "Remaining this year " + days + " days - " + hours + " hours - " + minutes + " mins and " + seconds + " secs and counting..";
+
+    setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
     }
+    return i;
+}
 
-    function checkTime(i) {
-        if (i < 10) {
-            i = "0" + i;
-        }
-        return i;
-    }
-
-    window.onload = function() {
-        startTime();
-    };
-
+window.onload = function() {
+    startTime();
+};
